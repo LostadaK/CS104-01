af8e0aeb0cdb9b117f5061bac00c129bf91211a4
LostadaK <146225329+LostadaK@users.noreply.github.com>
2023-09-27 12:40:38 -0400

    This is version 2 of my hello world program

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,8 @@
-print ("Hello my name is Lostada Kernisant")
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/losta/AppData/Local/Programs/Python/Python311/helloWorld.py
+Hello my name is Lostada Kernisant
+>>> 
+= RESTART: C:/Users/losta/AppData/Local/Programs/Python/Python311/helloWorld.py
+Hello my name is Lostada A. Kernisant
